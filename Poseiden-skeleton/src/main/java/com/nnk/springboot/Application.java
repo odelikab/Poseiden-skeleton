effd161921c0277912aa6b2cc617ad30809085df
odeli <misterkb5@hotmail.com>
2023-08-05 13:41:52 +0200

    logger setup

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/Application.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/Application.java
@@ -2,14 +2,12 @@ package com.nnk.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		BCryptPasswordEncoder pass = new BCryptPasswordEncoder();
-		System.out.println(pass.encode("abcde"));
+
 		SpringApplication.run(Application.class, args);
 	}
 
